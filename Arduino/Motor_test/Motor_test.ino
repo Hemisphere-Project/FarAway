c1a55b492c0be316b08bc459018c66074d1a8465
jeremie <jeremie@hemisphere-project.com>
2021-03-10 16:47:11 +0100

    motor ok & ota

--- Arduino/Motor_test/Motor_test.ino
+++ Arduino/Motor_test/Motor_test.ino
@@ -1,6 +1,7 @@
 
 #include <AccelStepper.h>
-
+// DEBUG
+#include "debug.h"
 
 
 int dirPin = 17;
@@ -8,30 +9,40 @@ int pulPin = 4;
 // AccelStepper stepper(1, pulPin, dirPin);
 AccelStepper stepper(AccelStepper::DRIVER, pulPin, dirPin);
 
+
 void setup()
 {
-  // Change these to suit your stepper if you want
-  stepper.setMaxSpeed(100000);
-  stepper.setAcceleration(50000);
-  stepper.moveTo(8000);
+  // SERIAL
+  Serial.begin(115200);
+
+  stepper.setMaxSpeed(100);
+  stepper.setAcceleration(200);//200
+  stepper.moveTo(200);
+
+  // stepper.setSpeed(200);
+
+  // WIFI
+  wifi_ota( "faraway-proto");
+  wifi_maxTry(20);
+  wifi_connect("kxkm-wifi", "KOMPLEXKAPHARNAUM");
 }
 
 void loop()
 {
-    // If at the end of travel go to the other end
-    if (stepper.distanceToGo() == 0)
-      stepper.moveTo(-stepper.currentPosition());
 
-    stepper.run();
+  wifi_loop();
+
+  if (stepper.distanceToGo() == 0){
+    delay(1000);
+    stepper.moveTo(-stepper.currentPosition());
+  }
+
+
+  stepper.run();
+
+  // Serial.println(stepper.currentPosition());
+
+  // stepper.runSpeed();
+
+
 }
-
-// void setup()
-// {
-//    stepper.setMaxSpeed(100000);
-//    stepper.setSpeed(100000);
-// }
-//
-// void loop()
-// {
-//    stepper.runSpeed();
-// }
